48b70a3fd820b2f740aef077e015cfaf46f1c47e
limbus <limbustwo@gmail.com>
2014-09-27 18:24:30 -0300

    criação da tela de lista das imagens, reestruturação dos pacotes... 2
    e também mudança do suporte das versões do android para 4 até 19

---
+++ TabletVox03f/src/com/example/tabletvox03f/management/perfil/ItemPerfilAdapter.java
@@ -0,0 +1,212 @@
+package com.example.tabletvox03f.management.perfil;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.tabletvox03f.R;
+import com.example.tabletvox03f.dal.Perfil;
+import com.example.tabletvox03f.dal.PerfilDAOSingleton;
+
+public class ItemPerfilAdapter extends BaseAdapter
+{
+	
+	private Context mContext;
+	private ArrayList<Perfil> perfis;
+	private LayoutInflater inflator;
+
+	public ItemPerfilAdapter(Context context, ArrayList<Perfil> lista)
+	{
+		this.mContext 	= context;
+		this.perfis 	= lista;
+		this.inflator 	= LayoutInflater.from(this.mContext);
+	}
+	
+	private class ViewHolder 
+	{
+	    protected TextView lblAutor, lblNomePerfil;
+	    protected ImageButton btnEdit, btnDelete, btnManage;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		View view = null;
+		
+		if (convertView == null)
+		{
+			view = inflator.inflate(R.layout.item_perfil, null);
+			
+			ViewHolder viewHolder 		= new ViewHolder();
+			viewHolder.lblAutor			= (TextView) view.findViewById(R.id.lblAutor);
+			viewHolder.lblNomePerfil	= (TextView) view.findViewById(R.id.lblNomePerfil);
+			viewHolder.btnEdit			= (ImageButton) view.findViewById(R.id.btnEdit);
+			viewHolder.btnDelete		= (ImageButton) view.findViewById(R.id.btnDelete);
+			viewHolder.btnManage		= (ImageButton) view.findViewById(R.id.btnManage);
+			
+//			viewHolder.btnDelete.setOnClickListener(new OnClickListener()
+//			{
+//				
+//				@Override
+//				public void onClick(View v)
+//				{
+//					Toast.makeText(ItemPerfilAdapter.this.mContext, "Você clicou no botão Deletar!", Toast.LENGTH_SHORT).show();
+//				}
+//			});
+			
+			
+			// para o click dos botões não "consumirem" o click do item do ListView
+			// sem essas linhas de código não funciona o click no item da lista
+			viewHolder.btnDelete.setFocusable(false);
+			viewHolder.btnEdit.setFocusable(false);
+			viewHolder.btnManage.setFocusable(false);
+			
+			view.setTag(viewHolder);
+		}
+		else
+			view = convertView;
+		
+		ViewHolder holder = (ViewHolder) view.getTag();
+		
+		final Perfil perfil = perfis.get(position);
+		
+		holder.lblAutor.setText(perfil.getAutor());
+		holder.lblNomePerfil.setText(perfil.getNome());
+
+		holder.btnManage.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				// setar PERFIL_ATIVO e abrir o activity ListaCategoriasDoPerfilActivity 
+				Toast.makeText(ItemPerfilAdapter.this.mContext, "Você clicou no botão Gerenciar!", Toast.LENGTH_SHORT).show();
+//				Utils.PERFIL_ATIVO = perfil;
+//				Intent intent = new Intent(ItemPerfilAdapter.this.mContext, ListaCategoriasDoPerfilActivity.class);
+//				ItemPerfilAdapter.this.mContext.startActivity(intent);
+			}
+		});
+		
+		holder.btnEdit.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				//Toast.makeText(ItemPerfilAdapter.this.mContext, "Você clicou no botão Editar!", Toast.LENGTH_SHORT).show();
+				// chamar form editar
+				Intent intent = new Intent(ItemPerfilAdapter.this.mContext, FormularioPerfilActivity.class);
+				intent.putExtra("tipo_form", false); // false para 'editar perfil'
+				
+				intent.putExtra("perfil", perfil);
+//				intent.putExtra("pfl_id", perfil.getId());
+//				intent.putExtra("pfl_nome", perfil.getNome());
+//				intent.putExtra("pfl_autor", perfil.getAutor());
+				((Activity) ItemPerfilAdapter.this.mContext).startActivityForResult(intent, 2);
+			}
+		});
+		
+		holder.btnDelete.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				AlertDialog.Builder builder = new AlertDialog.Builder(ItemPerfilAdapter.this.mContext);
+				
+				// conteudo da dialog
+				builder.setMessage("Tem Certeza?");
+				builder.setTitle("Confirmar Exclusão");
+				
+				// botoes
+				
+				// botao sim
+				builder.setPositiveButton("Sim", new DialogInterface.OnClickListener()
+				{
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which)
+					{
+						PerfilDAOSingleton.getInstance().excluirPerfil(perfil.getId());
+						
+						Toast.makeText(ItemPerfilAdapter.this.mContext, 
+						"Excluido com sucesso! ID: " + Integer.toString(perfil.getId()), 
+						Toast.LENGTH_SHORT).show();
+						
+						removeItem(perfil);
+						// refresh na lista
+						ItemPerfilAdapter.this.refresh();
+					}
+				});
+				
+				// botao não
+				builder.setNegativeButton("Não", new DialogInterface.OnClickListener()
+				{
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which)
+					{
+						Toast.makeText(ItemPerfilAdapter.this.mContext, "Cancelado!", Toast.LENGTH_SHORT).show();
+						return;
+					}
+				});				
+				
+				AlertDialog dialog = builder.create();
+				
+				dialog.show();
+				
+				
+//				Toast.makeText(ItemPerfilAdapter.this.mContext, "Você clicou no botão Deletar!", Toast.LENGTH_SHORT).show();
+			}
+		});		
+		
+		
+		return view;
+	}
+
+	@Override
+	public int getCount()
+	{
+		return perfis.size();
+	}
+
+	@Override
+	public Perfil getItem(int position) 
+	{
+		return perfis.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) 
+	{
+		return 0;
+	}
+	
+	public void removeItem(int position)
+	{
+		perfis.remove(position);
+	}
+	
+	public void removeItem(Perfil perfil)
+	{
+		perfis.remove(perfil);
+	}
+
+	public void refresh()
+	{
+		// informar o adapter que houve uma mudança na lista de views (tipo um refresh na lista)
+		notifyDataSetChanged();
+	}
+}
---
+++ TabletVox03f/src/com/example/tabletvox03f/management/perfil/SelecionarPerfilActivity.java
@@ -0,0 +1,109 @@
+package com.example.tabletvox03f.management.perfil;
+
+import java.util.ArrayList;
+
+import android.content.Intent;
+import android.text.Editable;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
+
+import com.example.tabletvox03f.R;
+import com.example.tabletvox03f.Utils;
+import com.example.tabletvox03f.dal.Perfil;
+import com.example.tabletvox03f.dal.PerfilDAOSingleton;
+import com.example.tabletvox03f.management.ListaManageActivity;
+
+public class SelecionarPerfilActivity extends ListaManageActivity
+{
+
+	@Override
+	protected void onCreateFilho()
+	{
+		
+	}
+	
+	@Override
+	protected void acaoDoEventoItemClick(AdapterView<?> parent, View v,
+			int position, long arg3)
+	{
+		// TODO Auto-generated method stub
+		Perfil pfl = (Perfil) parent.getItemAtPosition(position);
+		Toast.makeText(SelecionarPerfilActivity.this, "item: " + pfl.getNome(), Toast.LENGTH_SHORT).show();
+		// selecionar o perfil
+		selecionarPerfil(pfl);
+	}
+
+	// recarregar/carregar a lista ao voltar para esse activity
+	@Override
+	protected void onResume()
+	{
+		onResumeSuper();
+	}
+
+	// carregar todos os perfis
+	@SuppressWarnings("unchecked")
+	@Override
+	protected BaseAdapter carregarLista()
+	{
+		ArrayList<Perfil> lista = PerfilDAOSingleton.getInstance().getPerfis();
+		
+		return (new ItemPerfilAdapter(this, (ArrayList<Perfil>) lista.clone()));
+	}
+	
+	protected void carregarLista(ArrayList<Perfil> perfis)
+	{
+		lv.setAdapter(new ItemPerfilAdapter(this, (ArrayList<Perfil>) perfis.clone()));
+	}
+	
+	private void selecionarPerfil(Perfil perfil)
+	{
+		Utils.PERFIL_ATIVO = perfil;
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (resultCode == 1)
+			Toast.makeText(this, "Perfil incluido com sucesso!", Toast.LENGTH_SHORT).show();
+		else if (resultCode == 2)
+			Toast.makeText(this, "Perfil editado com sucesso", Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	protected void acaoDosEventosDoMenu(MenuItem item)
+	{
+		if (item.getItemId() == R.id.action_criar)
+		{
+			// chamar activity criar perfil
+			Intent intent = new Intent(this, FormularioPerfilActivity.class);
+			intent.putExtra("tipo_form", true); // true, para form do tipo 'criar' e false para form do tipo 'editar'
+			startActivityForResult(intent, 1);
+		}
+	}
+
+	@Override
+	protected String getOptionMenuTitle()
+	{
+		return "Criar Perfil";
+	}
+
+	@Override
+	protected int getMenuID()
+	{
+		return R.menu.um_action_add;
+	}
+
+	// buscar por nome e autor
+	@Override
+	protected void acaoDoEventoBuscar(Editable s)
+	{
+		String texto_para_pesquisa = s.toString();
+		carregarLista(PerfilDAOSingleton.getInstance().getPerfisByNomeOrAutor(texto_para_pesquisa, texto_para_pesquisa));
+	}
+
+}

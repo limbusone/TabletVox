86816b6cf80b06e5e05177b4df91a786bcaa69c3
leo_2 <leo_2@LEO-PC>
2014-11-15 00:20:48 -0200

    introduzindo constantes para organizar melhor o código

--- TabletVox03f/src/com/example/tabletvox03f/management/categoria/ItemCategoriaAdapter.java
+++ TabletVox03f/src/com/example/tabletvox03f/management/categoria/ItemCategoriaAdapter.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tabletvox03f.R;
+import com.example.tabletvox03f.Utils;
 import com.example.tabletvox03f.dal.FilesIO;
 import com.example.tabletvox03f.dal.assocImagemSom.AssocImagemSom;
 import com.example.tabletvox03f.dal.categoria.Categoria;
@@ -145,13 +146,13 @@ public class ItemCategoriaAdapter extends BaseAdapter
 				// verifica se o "parent" desse adapter é a lista de categorias
 				if (ItemCategoriaAdapter.this.mContext instanceof ListaCategoriasActivity)
 				{
-					intent.putExtra("tipo_form", 1);
+					intent.putExtra("tipo_form", Utils.FORM_ALTERAR);
 					((Activity) ItemCategoriaAdapter.this.mContext).startActivityForResult(intent, 2);
 				}
 				// verifica se o "parent" desse adapter é o formulario do perfil
 				else if (ItemCategoriaAdapter.this.mContext instanceof FormularioPerfilActivity)
 				{
-					intent.putExtra("tipo_form", 2);
+					intent.putExtra("tipo_form", Utils.FORM_ALTERAR_NP);
 					((Activity) ItemCategoriaAdapter.this.mContext).startActivityForResult(intent, 3);
 				}
 					
